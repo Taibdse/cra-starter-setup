4c6e6eca664e3a64345368fd3eabd8b0e5c4bb21
taibdse <buiductai232@gmail.com>
2023-02-01 12:04:05 +0700

    update import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
 import { RouterProvider } from "react-router-dom";
 import { router } from 'router';
 import LayoutProvider from 'provider/LayoutProvider';
+import './App.css';
 
 function App() {
   return (
